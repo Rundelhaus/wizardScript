e03e551b0ecdd67262c39ef249be9e8adbf177c8
takoage <dima30051999@mail.ru>
2022-01-21 19:47:40 +0700

    add console input, small code refactor

--- script.php
+++ script.php
@@ -1,9 +1,15 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-const DOMAIN = 'http://api.seowizard.ru/xmlrpc/?v=extended';
-$login = '';
-$token = '';
+const DOMAIN            = 'http://api.seowizard.ru/xmlrpc/?v=extended';
+const METHOD_LOGIN      = 'seowizard.loginpass';
+const METHOD_PROJ_LIST  = 'seowizard.project_list';
+const OUTPUT_FILE       = 'all_projects.txt';
+
+fwrite(STDOUT, "Enter your login: ");
+$login = trim(fgets(STDIN));
+fwrite(STDOUT, "Enter your token: ");
+$token = trim(fgets(STDIN));
 
 $ch = curl_init(DOMAIN);
 curl_setopt_array($ch, [
@@ -11,7 +17,7 @@ curl_setopt_array($ch, [
     CURLOPT_HTTPHEADER => [
         'Content-Type: text/xml'
     ],
-    CURLOPT_POSTFIELDS => xmlrpc_encode_request('seowizard.loginpass', [
+    CURLOPT_POSTFIELDS => xmlrpc_encode_request(METHOD_LOGIN, [
         $login,
         $token
     ]),
@@ -20,18 +26,25 @@ curl_setopt_array($ch, [
     CURLOPT_COOKIEFILE => 'cookies.txt',
 ]);
 
-curl_exec($ch);
-$projectListRequest = xmlrpc_encode_request('seowizard.project_list', null);
+$response = xmlrpc_decode(curl_exec($ch));
+if (is_array($response) && xmlrpc_is_fault($response)) {
+    fwrite(STDOUT, $response['faultString'] . PHP_EOL);
+} else {
+    $projectListRequest = xmlrpc_encode_request(METHOD_PROJ_LIST, null);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $projectListRequest);
+    $response = curl_exec($ch);
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, $projectListRequest);
+    $projectList = xmlrpc_decode($response, 'UTF-8');
+    if (xmlrpc_is_fault($projectList)) {
+        fwrite(STDOUT, $projectList['faultString'] . PHP_EOL);
+    } else {
+        $resFile = fopen(OUTPUT_FILE, 'w+');
+        foreach ($projectList as $project) {
+            $content = $project['domain'] . ' - ' . $project['sum_budget'];
+            fwrite($resFile, $content . PHP_EOL);
+        }
+        fclose($resFile);
 
-$ret = curl_exec($ch);
-$projectList = xmlrpc_decode($ret, 'UTF-8');
-
-$resFile = fopen('all_projects.txt', 'w+');
-
-foreach ($projectList as $project) {
-    $content = $project['domain'] . ' - ' . $project['sum_budget'];
-    fwrite($resFile, $content . PHP_EOL);
+        fwrite(STDOUT, "Budgets successfully fetched to " . OUTPUT_FILE . PHP_EOL);
+    }
 }
-fclose($resFile);
